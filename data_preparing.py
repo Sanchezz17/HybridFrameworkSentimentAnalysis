a92bdfe5c6a428a410495ea73eeb6c0dcef5cb96
Евгений <gekachan2000@gmail.com>
2022-06-25 18:00:50 +0500

    Refactoring data preparing

--- data_preparing.py
+++ data_preparing.py
@@ -4,12 +4,18 @@ from text_cleaning import clean_text
 from text_preprocessing import preprocess_text
 
 
-def prepare_data(dataset: pd.DataFrame):
+def prepare_data(dataset: pd.DataFrame) -> (pd.Series, pd.DataFrame, pd.Series):
     # Чистим текст от мусора, исправляем сленг, удаляем стоп-слова
-    dataset.text = dataset.text.apply(clean_text)
+    cleared_text = dataset.text.apply(clean_text)
 
     # Предварительная обработка: токенизация, стемминг, разметка частей речи
-    dataset = dataset.assign(keywords=dataset.text.apply(preprocess_text))
-    print(dataset)
+    # Инициализация списка ключевых слов текста и их свойств
+    keywords = cleared_text.apply(preprocess_text)
 
-    return dataset
+    X = pd.DataFrame(keywords.to_list())
+    print(X)
+
+    y = dataset.label
+    print(y)
+
+    return keywords, X, y
--- main.py
+++ main.py
@@ -2,11 +2,9 @@ import pandas as pd
 from sklearn.metrics import accuracy_score
 
 from data_preparing import prepare_data
-from text_cleaning import clean_text
 # Датасет - обзоры фильмов на IMDB на английском языке
 # https://www.kaggle.com/datasets/columbine/imdb-dataset-sentiment-analysis-in-csv-format
 from lexicon_based_sentiment_analysis import calculate_polarity_score_swn
-from text_preprocessing import preprocess_text
 
 train_dataset = pd.read_csv('IMDB dataset/Train.csv')
 test_dataset = pd.read_csv('IMDB dataset/Test.csv')
@@ -17,16 +15,17 @@ train_dataset = train_dataset.head(int(len(train_dataset) * (n / 100)))
 test_dataset = test_dataset.head(int(len(test_dataset) * (n / 100)))
 
 # Подготовка входных данных
-train_dataset = prepare_data(train_dataset)
-test_dataset = prepare_data(test_dataset)
+keywords_train, X_train, y_train = prepare_data(train_dataset)
+keywords_test, X_test, y_test = prepare_data(test_dataset)
 
 # Подход 1: Анализ настроений на основе лексикона (Lexicon-based sentiment analysis)
-y_pred_lexicon = test_dataset.keywords.apply(calculate_polarity_score_swn)
-y_real = test_dataset.label
+# Проблема подхода: ограничен списком помеченных в SentiWordNet слов
+y_pred_lexicon = keywords_test.apply(calculate_polarity_score_swn)
 # Вычисляем accuracy - долю правильных ответов алгоритма
-lexicon_accuracy = accuracy_score(y_real, y_pred_lexicon)
+lexicon_accuracy = accuracy_score(y_test, y_pred_lexicon)
 print(f"Lexicon-based accuracy: {lexicon_accuracy}")
 
 # Подход 2: Анализ настроений на основе классификации и использования набора уникальных слов как признаков
 # ML using bag of words as features
+# Проблема подхода: слишком много признаков, не масштабируем на большие наборы данных
 # toDo
