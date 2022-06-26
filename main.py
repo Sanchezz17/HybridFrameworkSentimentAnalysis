23e928370b9c864c7fef2f6adada509ade86d279
Евгений <gekachan2000@gmail.com>
2022-06-26 13:29:30 +0500

    Другие методы классификации

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from sklearn.metrics import accuracy_score
 
 from data_preparing import prepare_data, get_x_y
 from lexicon_based_sentiment_analysis import calculate_polarity_score_swn
-from ml_using_bag_of_words_as_features import knn_classification
+from ml_using_bag_of_words_as_features import knn_classification, svc_classification, \
+    naive_bayes_classification, dt_classification, classify_and_print_accuracy, svc_rbf_classification, \
+    random_forest_classification, ada_boost_classification, quadratic_discriminant_classification
 
 # Датасет - обзоры фильмов на IMDB на английском языке
 # https://www.kaggle.com/datasets/columbine/imdb-dataset-sentiment-analysis-in-csv-format
@@ -11,7 +13,7 @@ train_dataset = pd.read_csv('IMDB dataset/Train.csv')
 test_dataset = pd.read_csv('IMDB dataset/Test.csv')
 
 # Для быстрой проверки кода при разработке возьмем долю датасета в n процентов
-n = 5
+n = 1
 train_dataset = train_dataset.head(int(len(train_dataset) * (n / 100)))
 test_dataset = test_dataset.head(int(len(test_dataset) * (n / 100)))
 
@@ -23,17 +25,31 @@ keywords_test, keywords_test_counters, keywords_dataframe_test = prepare_data(te
 keywords_dataframe_test = keywords_dataframe_test.reindex(columns=keywords_dataframe_train.columns.tolist()).fillna(0)
 X_test, y_test = get_x_y(test_dataset, keywords_dataframe_test)
 
+# Для всех результатов вычисляем accuracy - долю правильных ответов алгоритма
+
 # Подход 1: Анализ настроений на основе лексикона (Lexicon-based sentiment analysis)
 # Проблема подхода: ограничен списком помеченных в SentiWordNet слов
 y_pred_lexicon = calculate_polarity_score_swn(keywords_test, keywords_test_counters)
-# Вычисляем accuracy - долю правильных ответов алгоритма
 lexicon_accuracy = accuracy_score(y_test, y_pred_lexicon)
 print(f"Lexicon-based accuracy: {lexicon_accuracy}")
 
 # Подход 2: Анализ настроений на основе классификации и использования набора уникальных слов как признаков
 # ML using bag of words as features
 # Проблема подхода: слишком много признаков, не масштабируем на большие наборы данных
-y_pred_ml = knn_classification(X_train, y_train, X_test)
-# Вычисляем accuracy - долю правильных ответов алгоритма
-ml_accuracy = accuracy_score(y_test, y_pred_ml)
-print(f"ML using bag of words as features accuracy: {ml_accuracy}")
+classify_and_print_accuracy(knn_classification, "KNN", X_train, y_train, X_test, y_test)
+
+classify_and_print_accuracy(svc_classification, "SVC", X_train, y_train, X_test, y_test)
+
+classify_and_print_accuracy(svc_rbf_classification, "SVC RBF", X_train, y_train, X_test, y_test)
+
+classify_and_print_accuracy(naive_bayes_classification, "Naive Bayes", X_train, y_train, X_test, y_test)
+
+classify_and_print_accuracy(dt_classification, "DT", X_train, y_train, X_test, y_test)
+
+classify_and_print_accuracy(random_forest_classification, "Random forest", X_train, y_train, X_test, y_test)
+
+classify_and_print_accuracy(ada_boost_classification, "AdaBoost", X_train, y_train, X_test, y_test)
+
+classify_and_print_accuracy(quadratic_discriminant_classification, "Quadratic discriminant", X_train, y_train, X_test, y_test)
+
+# Подход 3: Гибридный метод с оптимальным выбором признаков
--- ml_using_bag_of_words_as_features.py
+++ ml_using_bag_of_words_as_features.py
@@ -1,9 +1,62 @@
 import numpy as np
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier as KNN
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 
 
-def knn_classification(X_train: np.ndarray, y_train: np.ndarray, X_test: np.ndarray) \
-        -> np.ndarray:
+def knn_classification(X_train: np.ndarray, y_train: np.ndarray, X_test: np.ndarray) -> np.ndarray:
     model = KNN(n_neighbors=10)
+    return classification(model, X_train, y_train, X_test)
+
+
+def svc_classification(X_train: np.ndarray, y_train: np.ndarray, X_test: np.ndarray) -> np.ndarray:
+    model = SVC(kernel='linear', random_state=0)
+    return classification(model, X_train, y_train, X_test)
+
+
+def svc_rbf_classification(X_train: np.ndarray, y_train: np.ndarray, X_test: np.ndarray) -> np.ndarray:
+    model = SVC(kernel='rbf', random_state=0)
+    return classification(model, X_train, y_train, X_test)
+
+
+def naive_bayes_classification(X_train: np.ndarray, y_train: np.ndarray, X_test: np.ndarray) -> np.ndarray:
+    model = GaussianNB()
+    return classification(model, X_train, y_train, X_test)
+
+
+def dt_classification(X_train: np.ndarray, y_train: np.ndarray, X_test: np.ndarray) -> np.ndarray:
+    model = DecisionTreeClassifier(criterion='entropy', random_state=0)
+    return classification(model, X_train, y_train, X_test)
+
+
+def random_forest_classification(X_train: np.ndarray, y_train: np.ndarray, X_test: np.ndarray) -> np.ndarray:
+    model = RandomForestClassifier(max_depth=5, random_state=0)
+    return classification(model, X_train, y_train, X_test)
+
+
+def ada_boost_classification(X_train: np.ndarray, y_train: np.ndarray, X_test: np.ndarray) -> np.ndarray:
+    model = AdaBoostClassifier()
+    return classification(model, X_train, y_train, X_test)
+
+
+def quadratic_discriminant_classification(X_train: np.ndarray, y_train: np.ndarray, X_test: np.ndarray) -> np.ndarray:
+    model = QuadraticDiscriminantAnalysis()
+    return classification(model, X_train, y_train, X_test)
+
+
+def classification(model, X_train: np.ndarray, y_train: np.ndarray, X_test: np.ndarray) -> np.ndarray:
     model.fit(X_train, y_train)
     return model.predict(X_test)
+
+
+def classify_and_print_accuracy(method, method_name: str,
+                                X_train: np.ndarray, y_train: np.ndarray,
+                                X_test: np.ndarray, y_test: np.ndarray):
+    y_pred = knn_classification(X_train, y_train, X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    print(f"{method_name} ML using bag of words as features accuracy: {accuracy}")
